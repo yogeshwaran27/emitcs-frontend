b9b540e08c869fc56600801ce382f652e90093d7
yogeshwaran27 <50258859+yogeshwaran27@users.noreply.github.com>
2025-07-26 15:44:46 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:true
+    allowedHosts: ['ts.emitcms.com'], // Add your allowed host here
   },
 })
